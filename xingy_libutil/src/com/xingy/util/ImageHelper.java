25a744a46fdafa070477984d6d8204b1afec8cc9
xingyes <xingyao@vip.qq.com>
2015-07-30 18:18:42 +0800

    drawable － bitmap 改动

--- xingy_libutil/src/com/xingy/util/ImageHelper.java
+++ xingy_libutil/src/com/xingy/util/ImageHelper.java
@@ -233,7 +233,12 @@ public class ImageHelper {
 
 	public static Bitmap drawableToBitmap(Drawable drawable) {
 
-		Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565);
+        if (drawable instanceof BitmapDrawable)
+        {
+            BitmapDrawable bd = (BitmapDrawable) drawable;
+            return bd.getBitmap();
+        }
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565);
 		Canvas canvas = new Canvas(bitmap);
 		drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
 		drawable.draw(canvas);
